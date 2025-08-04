db63f95fec8526922bb62e4789f93f4e7c7e42f7
NamSangwon <nsw3265@naver.com>
2025-08-04 23:53:12 +0900

    Add : 스마트 컨트랙트 배포 DTO 생성 #1

---
+++ src/main/java/com/ddiring/Backend_BlockchainConnector/domain/dto/SmartContractDeployDto.java
@@ -0,0 +1,19 @@
+package com.ddiring.Backend_BlockchainConnector.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class SmartContractDeployDto {
+    @NotBlank(message="조각 투자 상품명을 입력하시오.")
+    private String name;
+
+    @NotBlank(message = "조각 투자 심볼을 입력하시오.")
+    private String symbol;
+
+    @NotBlank(message = "조각 투자 상품의 목표 금액을 입력하시오.")
+    private Long totalGoalAmount;
+
+    @NotBlank(message = "조각 투자 상품의 최소 투자 금액을 입력하시오.")
+    private Long minAmount;
+}
