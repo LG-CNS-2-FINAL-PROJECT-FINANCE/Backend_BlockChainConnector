d245068e2184523c81c7a7199400b6f94acedbec
NamSangwon <nsw3265@naver.com>
2025-08-08 18:34:27 +0900

    Fix #5 : 스마트 컨트랙트 주소 업데이트 요청 URL 수정

--- src/main/java/com/ddiring/Backend_BlockchainConnector/remote/deploy/RemoteProductService.java
+++ src/main/java/com/ddiring/Backend_BlockchainConnector/remote/deploy/RemoteProductService.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "RemoteProductService", path = "/api/product")
+@FeignClient(name = "RemoteProductService", path = "/api/product", url = "http://localhost:8081")
 public interface RemoteProductService {
     @PostMapping("/update/contract")
     ResponseEntity<Void> setContractAddress(@RequestBody UpdateContractAddressDto updateContractAddressDto);
