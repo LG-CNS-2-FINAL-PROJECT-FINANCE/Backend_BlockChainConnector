97cba3bcc230dd45a3d51fdb16f0fb9f91e8ece9
NamSangwon <nsw3265@naver.com>
2025-08-12 14:58:34 +0900

    Add #14 : 컨트롤러에 TradeDto 형식으로 입력 받기

--- src/main/java/com/ddiring/Backend_BlockchainConnector/api/open/SmartContractController.java
+++ src/main/java/com/ddiring/Backend_BlockchainConnector/api/open/SmartContractController.java
@@ -1,10 +1,7 @@
 package com.ddiring.Backend_BlockchainConnector.api.open;
 
 import com.ddiring.Backend_BlockchainConnector.common.dto.ApiResponseDto;
-import com.ddiring.Backend_BlockchainConnector.domain.dto.BalanceDto;
-import com.ddiring.Backend_BlockchainConnector.domain.dto.InvestmentDto;
-import com.ddiring.Backend_BlockchainConnector.domain.dto.SmartContractDeployDto;
-import com.ddiring.Backend_BlockchainConnector.domain.dto.SmartContractDeployResultDto;
+import com.ddiring.Backend_BlockchainConnector.domain.dto.*;
 import com.ddiring.Backend_BlockchainConnector.service.SmartContractService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -40,7 +37,7 @@ public class SmartContractController {
     }
 
     @PostMapping(value = "/trade")
-    public ApiResponseDto<?> tradeToken() {
+    public ApiResponseDto<?> tradeToken(@RequestBody @Valid TradeDto tradeDto) {
         return ApiResponseDto.defaultOK();
     }
 
