6de8af5b99b282b32fda548db2dea6737022dbbb
NamSangwon <nsw3265@naver.com>
2025-08-19 14:56:15 +0900

    Fix #14 : 토큰 거래 데이터 재구성에 따른 DTO 재구성

---
+++ src/main/java/com/ddiring/Backend_BlockchainConnector/domain/dto/trade/BuyInfo.java
@@ -0,0 +1,13 @@
+package com.ddiring.Backend_BlockchainConnector.domain.dto.trade;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BuyInfo {
+    private Long buyId;
+    private String buyerAddress;
+    private Long tokenAmount;
+}
