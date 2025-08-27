1fd54051208bc33bce2b0bd740bab891216dc09b
NamSangwon <nsw3265@naver.com>
2025-08-27 12:23:03 +0900

    Fix #28 : 다수 개의 투자 요청을 받을 수 있도록 DTO 수정

--- src/main/java/com/ddiring/Backend_BlockchainConnector/domain/dto/InvestmentDto.java
+++ src/main/java/com/ddiring/Backend_BlockchainConnector/domain/dto/InvestmentDto.java
@@ -1,26 +1,41 @@
 package com.ddiring.Backend_BlockchainConnector.domain.dto;
 
+import com.ddiring.contract.FractionalInvestmentToken;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InvestmentDto {
-    @NotNull(message = "투자 아이디를 입력하시오.")
-    @Positive(message = "투자 아이디는 양수로 구성되어야 합니다.")
-    private Long investmentId;
-
     @NotBlank(message = "프로젝트 아이디를 입력하시오.")
     private String projectId;
 
-    @NotBlank(message = "투자자 지갑 주소를 입력하시오.")
-    private String investorAddress;
+    @NotNull
+    private List<InvestInfo> investInfoList;
 
-    @NotNull(message = "투자할 토큰 개수를 입력하시오.")
-    @Positive(message = "투자할 토큰 개수는 1개 이상이어야 합니다.")
-    private Long tokenAmount;
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class InvestInfo {
+        @NotNull(message = "투자 아이디를 입력하시오.")
+        @Positive(message = "투자 아이디는 양수로 구성되어야 합니다.")
+        private Long investmentId;
+
+        @NotBlank(message = "투자자 지갑 주소를 입력하시오.")
+        private String investorAddress;
+
+        @NotNull(message = "투자할 토큰 개수를 입력하시오.")
+        @Positive(message = "투자할 토큰 개수는 1개 이상이어야 합니다.")
+        private Long tokenAmount;
+    }
+
+
 }
